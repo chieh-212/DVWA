d11f16c45a94ca193d81385cfb7d7090dc5d5fee
chieh-212 <fengnight0212@gmail.com>
2025-08-04 20:30:01 +0800

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,4 +1,5 @@
 <?php
+die('我的是 config.inc.php');
 
 # If you are having problems connecting to the MySQL database and all of the variables below are correct
 # try changing the 'db_server' variable from localhost to 127.0.0.1. Fixes a problem due to sockets.
